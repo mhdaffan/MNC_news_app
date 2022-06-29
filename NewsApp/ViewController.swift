8a0cc6a8e56939bde34bf0272951326523662fbb
Muhammad Affan <muhammad.affan@styletheory.co>
2022-06-29 11:59:04 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Muhammad Affan on 29/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
